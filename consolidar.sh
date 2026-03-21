ba5b12838e9b007b3f9e045523350978c0d1f9b4
Cami-loiacono <camiloia.cole@gmail.com>
2026-03-21 01:17:58 -0300

    Opcion 3, 4 y 5(en proceso)

--- consolidar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for archivo in entrada/*; do
-cat "$archivo" >> ~/EJ1/EPNro1/salida/$FILENAME
-mv $archivo procesado
-echo -e "\033[32mArchivo: $archivo , copiado y traladado a carpeta 'procesado'\033[0m"
-done
-
